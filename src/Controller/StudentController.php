c2e91835333440c4b74fa8cfcea80e72eba0e21a
Nada-bkh <nada.benkhlifa@esprit.tn>
2023-09-27 12:44:19 +0100

    Atelier 2

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/students', name: 'student_index')]
+
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'message' => 'Bonjour mes étudiants',
+        ]);
+    }
+}
